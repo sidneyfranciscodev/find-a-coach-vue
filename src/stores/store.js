b11735f95829c662dcaa35ca45626717d0be29d3
Sidney <sidneyfranciscodev@gmail.com>
2025-08-20 03:45:24 +0200

    create store & add dummy data & getters

---
+++ src/stores/store.js
@@ -0,0 +1,31 @@
+import { defineStore } from 'pinia';
+
+export const useStore = defineStore('store', {
+  state: () => ({
+    coaches: [
+      {
+        id: 'c1',
+        firstName: 'Max',
+        lastName: 'Mad',
+        areas: ['frontend', 'backend'],
+        description: "I'm Max and I've worked as a freelance web developer for years.",
+        hourlyRate: 30,
+      },
+      {
+        id: 'c2',
+        firstName: 'Julie',
+        lastName: 'Jone',
+        areas: ['frontend', 'career'],
+        description: "I'm Julie I am a senior developer in a big company.",
+        hourlyRate: 30,
+      }
+    ],
+  }),
+
+  getters: {
+    coaches: state => state.coaches,
+    hasCoaches: state => state.coaches && state.coaches.lenght > 0,
+  },
+
+  actions: {},
+});
