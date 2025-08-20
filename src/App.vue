222f9e74919108a4d7715bdff8c3063c2fd50994
Sidney <sidneyfranciscodev@gmail.com>
2025-08-20 03:13:56 +0200

    create TheHeader.vue component & add to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup></script>
+<script setup>
+import TheHeader from './components/layout/TheHeader.vue';
+</script>
 
 <template>
+  <the-header></the-header>
   <router-view></router-view>
 </template>
 
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,75 @@
+<template>
+  <header>
+    <nav>
+      <h1><router-link to="/">Find a Coach</router-link></h1>
+      <ul>
+        <li><router-link to="/coaches">All Coaches</router-link></li>
+        <li><router-link to="/requests">Requests</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+$link-color: #f391e3;
+
+@mixin menu-display($content: center){
+  display: flex;
+  justify-content: $content;
+  align-items: center;
+}
+
+a {
+  text-decoration: none;
+  color: $link-color;
+  display: inline-block;
+  padding: 0.75rem 1.5rem;
+  border: 1px solid transparent;
+
+  &:active,
+  &:hover,
+  &.router-link-active {
+   border: 1px solid $link-color;
+  }
+}
+
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #3d008d;
+  @include menu-display();
+
+  nav {
+    width: 90%;
+    margin: auto;
+    @include menu-display(space-between);
+
+    h1 {
+      margin: 0;
+
+      a {
+        color: white;
+        margin: 0;
+
+        &:active,
+        &:hover,
+        &.router-link-active {
+          border-color: transparent;
+        }
+      }
+    }
+
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      @include menu-display();
+
+      li {
+        margin: 0 0 0.5rem;
+      }
+    }
+  }
+}
+</style>
