1676965430f9971c9a6ab36db088d115a3fe0637
Sidney <sidneyfranciscodev@gmail.com>
2025-08-21 01:35:31 +0200

    create CoachItem component

---
+++ src/components/layout/coaches/CoachItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
+    <div>
+      <span v-for="area in areas" :key="area">{{ area }}</span>
+    </div>
+    <div class="actions">
+      <router-link to="/coaches/c1/contact">Contact</router-link>
+      <router-link to="/coaches/c1">Details</router-link>
+    </div>
+  </li>
+</template>
+
+<script setup>
+import { useStore } from '@/stores/strore.js';
+
+defineProps(['fullName', 'rate', 'areas'])
+const store = useStore();
+</script>
+
+
+<style lang="scss" scoped>
+li {
+  h3 {
+    font-size: 1.5rem;
+  }
+
+  h3,
+  h4,
+  div {
+    margin: 0.5rem 0;
+  }
+
+  .actions {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+</style>
