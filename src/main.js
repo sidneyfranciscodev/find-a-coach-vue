52e6b7757af621dda2d9e75c9a956535642d8b98
Sidney <sidneyfranciscodev@gmail.com>
2025-08-21 03:10:23 +0200

    register base-button as global component

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import BaseCard from './components/ui/BaseCard.vue'
+import BaseButton from './components/ui/BaseButton.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.component('base-card', BaseCard)
+app.component('base-button', BaseButton)
 
 app.mount('#app')
