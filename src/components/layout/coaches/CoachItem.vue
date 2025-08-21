c8490aeb86bea1c3a22c97d7f265bff0105ec7ac
Sidney <sidneyfranciscodev@gmail.com>
2025-08-21 02:13:50 +0200

    update CoachItem props & router-links

--- src/components/layout/coaches/CoachItem.vue
+++ src/components/layout/coaches/CoachItem.vue
@@ -6,19 +6,21 @@
       <span v-for="area in areas" :key="area">{{ area }}</span>
     </div>
     <div class="actions">
-      <router-link to="/coaches/c1/contact">Contact</router-link>
-      <router-link to="/coaches/c1">Details</router-link>
+      <router-link :to="contactLink">Contact</router-link>
+      <router-link :to="detailsLink">Details</router-link>
     </div>
   </li>
 </template>
 
 <script setup>
-import { useStore } from '@/stores/strore.js';
+import { computed, route } from 'vue';
 
-defineProps(['fullName', 'rate', 'areas'])
-const store = useStore();
+const props = defineProps(['id', 'firstName', 'lastName', 'rate', 'areas'])
+
+const fullName = computed(() => props.firstName + '' + props.lastName);
+const contactLink = computed(() => route.path + '/' + props.id + '/contact');
+const detailsLink = computed(() => route.path + '/' + props.id);
 </script>
-
 
 <style lang="scss" scoped>
 li {
