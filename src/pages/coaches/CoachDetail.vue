03ce163482ea065bce305fe4fd1468637f42893b
Sidney <sidneyfranciscodev@gmail.com>
2025-08-21 04:27:21 +0200

    update CoachDetail.vue & fix rate value at CoachesList.vue

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,5 +1,41 @@
 <template>
-  DETAILS FOR COACH
-  <router-view></router-view>
-  <router-link to="/coaches/c1/contact">Contact</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>{{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button link :to="contactLnk">Contact</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
+
+<script setup>
+import { useStore } from '@/stores/store';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const store = useStore();
+const route = useRoute();
+
+const props = defineProps(['id']);
+const selectedCoach = computed(() => store.allCoaches.find(coach => coach.id === props.id));
+
+const fullName = computed(() => selectedCoach.firstName + ' ' + selectedCoach.lastName);
+const contactLnk = computed(() => route.path +  '/contact');
+const areas = computed(() => selectedCoach.areas);
+const description = computed(() => selectedCoach.description)
+const rate = computed(() => selectedCoach.hourlyRate)
+</script>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -12,7 +12,7 @@
         :first-name="coach.firstName"
         :last-name="coach.lastName"
         :areas="coach.areas"
-        :rate="coach.rate"
+        :rate="coach.hourlyRate"
         ></coach-item>
       </ul>
       <h3 v-else>No coaches found.</h3>
