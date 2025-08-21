a78f53f890c52a512d5861f07c2ad825413f4b11
Sidney <sidneyfranciscodev@gmail.com>
2025-08-21 03:28:25 +0200

    create BaseBadge.vue & register as global component

---
+++ src/components/ui/BaseBadge.vue
@@ -0,0 +1,35 @@
+<template>
+  <span class="badge" :class="type">{{ text }}</span>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps(['text', 'title']);
+const text = computed(() => props.title.toUpperCase())
+</script>
+
+<style scoped>
+.badge {
+  background-color: #ccc;
+  color: #252525;
+  border-radius: 30px;
+  padding: 0.5rem 1.5rem;
+}
+
+.frontend {
+  background-color: #3d008d;
+  color: white;
+}
+
+.backend {
+  background-color: #71008d;
+  color: white;
+}
+
+.career {
+  background-color: #8d006e;
+  color: white;
+}
+</style>
+
