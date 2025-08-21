f1cfdebacff9ddf81330f000cfd94bef741734a8
Sidney <sidneyfranciscodev@gmail.com>
2025-08-21 02:49:33 +0200

    add base-badge to CoachesList.vue

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,20 +1,22 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item v-for="coach in filteredCoaches" :key="coach.id"
-      :id="coach.id"
-      :first-name="coach.firstName"
-      :last-name="coach.lastName"
-      :areas="coach.areas"
-      :rate="coach.rate"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <button>Refresh</button>
+        <router-link to="/register">Register as Coach</router-link>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item v-for="coach in filteredCoaches" :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :areas="coach.areas"
+        :rate="coach.rate"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
