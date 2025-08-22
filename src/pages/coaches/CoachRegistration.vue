896623cfc381f98fde10f1e34a788ac46e8b5cc0
Sidney <sidneyfranciscodev@gmail.com>
2025-08-22 16:08:43 +0200

    add: CoachForm to CoachRegistration.vue

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,6 +1,12 @@
 <template>
   <section>
-    <h2>Register as a coach now!</h2>
-    
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
   </section>
 </template>
+
+<script setup>
+import CoachForm from '@/components/coaches/CoachForm.vue';
+</script>
