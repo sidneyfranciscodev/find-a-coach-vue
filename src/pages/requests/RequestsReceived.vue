4b7f5957883f09866aeb9a2b9ea29766e1346c8b
Sidney <sidneyfranciscodev@gmail.com>
2025-08-22 21:01:29 +0200

    add template, styles and functionality to RequestsReceived.vue

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,3 +1,40 @@
 <template>
-  REQUESTS
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <li></li>
+      </ul>
+      <h3 v-else>You haven't received any requests yet</h3>
+    </base-card>
+  </section>
 </template>
+
+<script setup>
+import { useStore } from "@/stores/store";
+import { computed } from "vue";
+
+const store = useStore();
+
+const receivedRequests = computed(() => store.requests);
+const hasRequests = computed(() => store.hasRequests);
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
