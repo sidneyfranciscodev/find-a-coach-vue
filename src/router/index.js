bcc7d2ec7753463d72ffdd7f9acd1fbc9f017575
Sidney <sidneyfranciscodev@gmail.com>
2025-08-29 19:52:37 +0200

    add: route for UserAuth.vue

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,13 @@ import CoachesList from '@/pages/coaches/CoachesList.vue'
 import CoachRegistration from '@/pages/coaches/CoachRegistration.vue'
 import ContactCoach from '@/pages/requests/ContactCoach.vue'
 import RequestsReceived from '@/pages/requests/RequestsReceived.vue'
+import UserAuth from '@/pages/auth/UserAuth.vue'
 import NotFound from '@/pages/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', redirect: '/coaches'},
+    { path: '/', redirect: '/coaches' },
     { path: '/coaches', component: CoachesList },
     { path: '/coaches/:id',
       component: CoachDetail,
@@ -18,9 +19,10 @@ const router = createRouter({
       children: [
       { path: 'contact', component: ContactCoach },
     ]},
-    { path: '/register', component: CoachRegistration},
-    { path: '/requests', component: RequestsReceived},
-    { path: '/:notFound(.*)', component: NotFound},
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/auth', component: UserAuth },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
 })
 
