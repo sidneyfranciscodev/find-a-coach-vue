dc5645377c8bd936163b23f7b579e6c3a6acab7e
Sidney <sidneyfranciscodev@gmail.com>
2025-08-31 14:20:56 +0200

    add: navigation guards to '/register' & '/requests'

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import ContactCoach from '@/pages/requests/ContactCoach.vue'
 import RequestsReceived from '@/pages/requests/RequestsReceived.vue'
 import UserAuth from '@/pages/auth/UserAuth.vue'
 import NotFound from '@/pages/NotFound.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,11 +20,23 @@ const router = createRouter({
       children: [
       { path: 'contact', component: ContactCoach },
     ]},
-    { path: '/register', component: CoachRegistration },
-    { path: '/requests', component: RequestsReceived },
-    { path: '/auth', component: UserAuth },
+    { path: '/register', component: CoachRegistration, meta: { requiresAuth: true } },
+    { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
+    { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
     { path: '/:notFound(.*)', component: NotFound },
   ],
-})
+});
 
-export default router
+router.beforeEach(function(to, _, next) {
+  const authStore = useAuthStore();
+  
+  if (to.meta.requiresAuth && !authStore.isLoggedIn) {
+    next('/auth');
+  } else if (to.meta.requiresUnauth && authStore.isLoggedIn) {
+    next('/coaches');
+  } else {
+    next();
+  }
+});
+
+export default router;
